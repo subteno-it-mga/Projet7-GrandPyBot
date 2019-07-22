1224dfaa255ece80c7768d1d694c429eebc2362d
Martin <martin.gaucher@subteno-it.fr>
2019-07-22 17:04:17 +0200

    add a refresh button to reload the page

--- static/custom.js
+++ static/custom.js
@@ -1,8 +1,6 @@
-$(window).on("load",function(){
-    console.log("ça passe là dedans")
-        
-})
 $(document).ready(function(){
-
-    $('#main_page').show();
+    $('#refresh').on("click",function(){
+        location.reload();
+    })
+    
 });
